fd7d606e8c55c9b0621a006bda89098c59c6e71c
jennifer-pecky <ugwojennifer7@gmail.com>
2023-08-28 23:57:30 +0100

    completed home page

--- src/components/oother/HomeUi.jsx
+++ src/components/oother/HomeUi.jsx
@@ -4,19 +4,19 @@ export default function HomeUi() {
     <>
       <div>
         <section className="">
-          <div className="mt-10 flex flex-col md:flex-row justify-center items-center space-y-4 md:space-y-0 md:space-x-10 abt-us">
+          <div className="mt-10 flex flex-col md:flex-row justify-center items-center space-y-4 md:space-y-0  abt-us">
             <div className="text-left w-[45%] my-txt">
-              <h1 className="text-4xl font-bold font-semibold text-[#8B4513]">
+              <h1 className="text-4xl  font-semibold text-[#8B4513]">
                 ABOUT US
               </h1>
 
-              <p className="text-xl text-[14px] md:text-left leading-[2] ">
+              <p className="md:text-[18px] text-[13px] text-left leading-[2] ">
                 Welcome to Adventura, your gateway to a world <br /> of
                 interactive storytelling and boundless imagination. <br /> We
                 are a passionate team of <br />
                 creators, developers and storytellers who <br /> believes in the
                 power of <br />
-                narratives that comes to life through your chioces. <br />
+                narratives that comes to life through your choices. <br />
               </p>
             </div>
 
@@ -26,7 +26,7 @@ export default function HomeUi() {
           </div>
         </section>
         <div className="flex flex-col md:flex-row justify-center items-center space-y-10 md:space-y-0 md:space-x-20 h-screen">
-          <div className="card">
+          <div className="card md:w-[55%]">
             <span className="title">Leave a Comment</span>
             <form className="form">
               <div className="group">
@@ -56,7 +56,7 @@ export default function HomeUi() {
               <button type="submit">Submit</button>
             </form>
           </div>
-          <div className="flex flex-col justify-center items-center md:items-start max-w-lg">
+          <div className="flex flex-col justify-center items-center md:items-start max-w-lg  w-[55%]">
             <h1 className="text-[#0e0d09] font-bold text-2xl text-center md:text-left">
               Connect with us
             </h1>
