e6d538bff36c236d8ae75a9a7dc530c270e8b81e
jennifer-pecky <ugwojennifer7@gmail.com>
2023-09-03 15:40:40 +0100

    worked on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -29,7 +29,7 @@ export default function Header() {
         className={` flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-[#F5F5F5]  text-sm py-3 sm:py-0 ${sticky}`}
       >
         <nav
-          className="relative max-w-7xl w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8"
+          className="relative  w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8"
           aria-label="Global"
         >
           <div className="flex items-center justify-between">
@@ -47,28 +47,6 @@ export default function Header() {
                 aria-controls="navbar-collapse-with-animation"
                 aria-label="Toggle navigation"
               >
-                {/* <svg
-                  className="hs-collapse-open:hidden w-4 h-4"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
-                  />
-                </svg>
-                <svg
-                  className="hs-collapse-open:block hidden w-4 h-4"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
-                </svg> */}
-
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
